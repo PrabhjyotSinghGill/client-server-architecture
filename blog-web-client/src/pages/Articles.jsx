a3a379893f7af2cdc71f2539a31c1cbc4dbfd0c5
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-01-23 20:51:01 -0500

    Axios : axios library helps frontend to send network requests to backend.

--- blog-web-client/src/pages/Articles.jsx
+++ blog-web-client/src/pages/Articles.jsx
@@ -1,21 +1,38 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./Content.jsx";
 import NotFound from "./NotFound.jsx";
+import axios from "axios";
+import CommentsList from "../components/CommentsList.jsx";
 
 const Articles = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams(); //used from react-router-dom library
-  const article = articles.find((article) => article.name === articleId);
 
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(
+        `http://localhost:8000/api/articles/${articleId}`
+      );
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+    loadArticleInfo();
+  }, []);
+
+  const article = articles.find((article) => article.name === articleId);
   if (!article) {
     return <NotFound></NotFound>;
   }
-  
+
   return (
     <div>
       <h1>{article.title}</h1>
+      <p>his article has {articleInfo.upvotes} upvote(s)</p>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments}></CommentsList>
     </div>
   );
 };
